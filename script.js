d64b05a3968da234f7f97461f75dd2e859de9d4c
KHmamoune <108459491+KHmamoune@users.noreply.github.com>
2022-07-06 18:53:42 +0100

    made the page functional
    -hovering the mouse over the grid will change the color of tht tiles.
    -added an "edit size" button to change the grid size to 1-100 tiles.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const grid = document.querySelector(".grid")
+const grid = document.createElement("div")
+grid.classList.add("grid")
+document.body.appendChild(grid)
 
 function createGrid(num) {
   for (let i=1; i<=num; i++) {
     const element = document.createElement("div")
+    element.classList.add("div")
     grid.appendChild(element)
   }
+  
+  document.querySelectorAll(".div").forEach(item => {
+    item.addEventListener("mouseenter", () => {
+    item.setAttribute("style", "background: black;")
+    })
+  })
 }
 
 createGrid(256)
+
+document.querySelector(".btn").addEventListener("click", () => size())
+
+function size() {
+  let size
+  do{
+    size = window.prompt("enter the size of the grid", String)
+  }while(size<1 || size>100)
+
+  document.querySelectorAll(".div").forEach(item => item.remove())
+  createGrid(size*size)
+  grid.style.gridTemplateColumns = "repeat(" + size + ",1fr)"
+}
